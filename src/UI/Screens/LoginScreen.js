d9a83dfd73baf9d0fe98ba63646234b3430ff8eb
Jazpalacios <drpalaciosjahaziel@gmail.com>
2022-11-16 13:41:12 -0300

    Intalacion de prop-types y react-navigation

--- src/UI/Screens/LoginScreen.js
+++ src/UI/Screens/LoginScreen.js
@@ -1,1 +1,9 @@
 import React from "react";
+
+export function Login() {
+  return (
+    <>
+      <h1>Bienvenido a Panioleta </h1>
+    </>
+  );
+}
--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { View, Text } from "react-native";
+import { createStackNavigator, createAppContainer } from "react-navigation";
 import { Button } from "./UI/components/Button";
+import { Login } from "./UI/Screens/LoginScreen";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
